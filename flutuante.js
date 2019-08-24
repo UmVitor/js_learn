bdd4849d6ffb7735f376c47af9037974ab6ae5d6
Vitor <vittor_barreto@hotmail.com>
2019-08-24 17:27:15 -0300

    second commit

--- flutuante.js
+++ flutuante.js
@@ -1,8 +1,10 @@
-//Os números em JavaScript têm muita precisão e podem se aproximar bastante de 0.1 . Mas o fatode esse número não poder ser representado de forma exata pode causar problemas. Considere este código:
+//Os números em JavaScript têm muita precisão e podem se aproximar bastante de 0.1.
+//Mas o fatode esse número não poder ser representado de forma exata pode causar problemas. Considere este código:
 
 var x = .3 – .2; // trinta centavos menos 20 centavos
 var y = .2 – .1; // vinte centavos menos 10 centavos
 x == y // => falso: os dois valores não são os mesmos!
 x == .1 // => falso: .3-.2 não é igual a .1
 y == .1 // => verdadeiro: .2-.1 é igual a .1
+
 
