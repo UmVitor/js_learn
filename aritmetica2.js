9fc2c16ec4520ce2e782f6e61e6cc11a39686d86
Vitor <vittor_barreto@hotmail.com>
2019-09-04 20:09:15 -0300

    Second change

--- aritmetica2.js
+++ aritmetica2.js
@@ -56,3 +56,41 @@ switch(n) {
 //cificam apenas o ponto de partida do código desejado; elas não especificam ponto final algum. Na
 //ausência de instruções break , uma instrução switch começa a executar seu bloco de código no rótulo
 //case correspondente ao valor de sua expressão e continua a executar as instruções até atingir o final
+//do bloco. Em raras ocasiões, é útil escrever código como esse, que “passa” de um rótulo case para
+//o seguinte, mas 99% das vezes deve-se tomar o cuidado de finalizar cada case com uma instrução
+//break . (Entretanto, ao usar switch dentro de uma função, pode-se utilizar uma instrução return , em
+//vez de uma instrução break . Ambas servem para finalizar a instrução switch e impedir que a execução
+//passe para o próximo case .)
+//Aqui está um exemplo mais realista da instrução switch ; ele converte um valor em uma string de um
+//modo que depende do tipo do valor:
+function convert(x) {
+	switch(typeof x) {
+		case 'number': // Converte o número para um inteiro hexadecimal
+			return x.toString(16);
+		case 'string': // Retorna a string colocada entre apóstrofos
+			return '"' + x + '"';
+		default: // Converte qualquer outro tipo da maneira usual
+			return String(x);
+	}
+}
+
+//
+//Note que, nos dois exemplos anteriores, as palavras-chave case são seguidas por literais numéricas e
+//strings literais, respectivamente. É assim que a instrução switch é mais frequentemente utilizada na
+//prática, mas note que o padrão ECMAScript permite que cada case seja seguido por uma expressão
+//arbitrária.
+//A instrução switch avalia primeiro a expressão que vem após a palavra-chave switch e depois avalia
+//as expressões case , na ordem em que aparecem, até encontrar um valor que coincida 1 . O case coin-
+//cidente é determinado usando-se o operador de identidade === e não o operador de igualdade == , de
+//modo que as expressões devem coincidir sem qualquer conversão de tipo.
+//Como nem todas as expressões case são avaliadas sempre que a instrução switch é executada, você
+//deve evitar o uso de expressões case que contenham efeitos colaterais, como chamadas de função
+//ou atribuições. O caminho mais seguro é simplesmente limitar suas expressões case às expressões
+//constantes.
+//Co nforme explicado anteriormente, se nenhuma das expressões case corresponde à expressão
+//switch , a instrução switch começa a executar seu corpo na instrução rotulada como default: .
+//Se não há rótulo default: , a instrução switch pula seu corpo completamente. Note que, nos
+//exemplos anteriores, o rótulo default: aparece no final do corpo de switch , após todos os rótulos
+//case . Esse é um lugar lógico e comum para ele, mas pode aparecer em qualquer lugar dentro do
+//corpo da instrução.
+//
